eb5cf549760d4901e7d89ba6e023fd480ac0a41f
Abhijit Bhadoria <abhijit.bhadoria@M-FVFG91RXQ05N.local>
2025-08-01 19:52:17 +0530

    bootstrap

---
+++ src/test/java/springai/text_to_image/TextToImageApplicationTests.java
@@ -0,0 +1,13 @@
+package springai.text_to_image;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextToImageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
